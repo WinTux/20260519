ffe83994d965ce044398832b0c605196bf82f773
rusok <rusok.software@gmail.com>
2026-05-27 19:06:18 -0400

    Usando secretos mediante variables de entorno

--- src/main/java/com/wintux/ejemplogit/HolaController.java
+++ src/main/java/com/wintux/ejemplogit/HolaController.java
@@ -1,11 +1,13 @@
 package com.wintux.ejemplogit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HolaController {
-
+    @Value("${DB_PASSWORD}")
+    private String password;
     @GetMapping("/hola")
     public String saludar() {
         return "Hola desde Spring Boot!!";
@@ -13,5 +15,10 @@ public class HolaController {
     @GetMapping("/adios")
     public String despedirse() {
         return "Hasta luego! desde Spring Boot!! usando nueva rama (ej. Pull request)";
+    }
+    @GetMapping("/hola2")
+    public String saludaPersonalizado() {
+        System.out.println("El valor de la ENV es: "+password);
+        return "Hola "+password+", desde Spring Boot!!";
     }
 }
